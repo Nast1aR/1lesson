64fb7dbf34fb713f0e5c253ed0b439b2fc3d244d
nastya <sinseana0@gmail.com>
2023-05-29 23:16:32 +0300

    2task

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 #numbers
-n1=int(input("Number1:"))
-n2=int(input("Number2:"))
-n3=int(input("Number3:"))
+#n1=int(input("Number1:"))
+#n2=int(input("Number2:"))
+#n3=int(input("Number3:"))
+
+#choice=int(input("1- maximum or 2= minimum or 3- average"))
+#if choice == 1:
+#    maximum = max(n1, n2, n3)
+#     print("maximum: ", maximum)
+# elif choice == 2:
+#     minimum = min(n1, n2, n3)
+#     print("minimum: ", minimum)
+# elif choice == 3:
+#     average = (n1 + n2 + n3) / 3
+#     print("average: ", average)
+# else:
+#     print("error")
+
+#2 задание
+meters int(input("ammout:"))
